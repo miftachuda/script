c4eebd42a6b99bbaaed23af16ac8d82fb238e800
Miftachul Huda <miftachulcc@gmail.com>
2022-08-11 19:54:40 +0700

    working

--- port.sh
+++ port.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 port() {
     if hash netstat 2>/dev/null; then
-        netstat -tulpn | grep :$1 
+        echo "getting port ${1} running"
+        netstat -tulpn | grep :$1
     else
         apt-get net-tools
-        netstat -tulpn | grep :$1 
+        netstat -tulpn | grep :$1
     fi
 }
-port
+port $@
